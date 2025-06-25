3ec55309a83f2cdf935ee08598069645831eab49
Xavier Abril <xaiken99@gmail.com>
2025-06-25 09:22:37 +0200

    reflection e entity app

---
+++ ConsoleReflection/Car.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleReflection
+{
+    public class Car
+    {
+        //reflection:capacita di aprire una classe qualsiasi e vedere i metodi pubblici di quella classe
+        public string Modello { get; set; }
+        public string Marca { get; set; }
+
+
+        public void Start()
+        {
+
+
+
+        }
+
+        public void Stop()
+
+        {
+
+        }
+
+
+
+    }
+}
---
+++ ConsoleReflection/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleReflection;
+
+Console.WriteLine("Hello, World!");
+
+//reflection utizzata per ottenere le classi, metodi e proprietà 
+
+var macchina = new Car();
+
+Type type = typeof(Car);
+
+foreach (var property in type.GetProperties())
+{
+    Console.WriteLine($"Property: {property.Name}- type: {property.PropertyType}");
+}
+
+foreach (var method in type.GetMethods())
+{
+    Console.WriteLine($"Method: {method.Name}- type: {method.ReturnType}");
+}
