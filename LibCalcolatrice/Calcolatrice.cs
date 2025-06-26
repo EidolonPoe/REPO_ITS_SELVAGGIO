431e5ac41a3b37525b54721c5809c5102f61cb96
Xavier Abril <xaiken99@gmail.com>
2025-06-26 16:47:58 +0200

    2 test

---
+++ LibCalcolatrice/Calcolatrice.cs
@@ -0,0 +1,20 @@
+﻿namespace LibCalcolatrice
+{
+    public class Calcolatrice
+    {
+
+        public int Somma(int numero1, int numero2)
+        {
+            return (numero1 + numero2);
+        }
+
+
+        public  float Divisione(float numero1, float numero2)
+        {
+            return (numero1 / numero2);
+        }
+
+
+
+    }
+}
---
+++ TestCalcolatrice/Test1.cs
@@ -0,0 +1,27 @@
+﻿using LibCalcolatrice;
+
+namespace TestCalcolatrice
+{
+    [TestClass]
+    //decoratori per consentire a visual studio chhe e un test
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+
+            var calc = new Calcolatrice();
+            var risultato = calc.Somma(3, 5);
+            Assert.AreEqual(8,risultato);
+        }
+
+        [TestMethod]
+        public void TestDivisione()
+        {
+
+            var calc = new Calcolatrice();
+            var risultato = calc.Divisione(5, 2);
+            Assert.AreEqual(4, risultato);
+        }
+    }
+}
